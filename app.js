ff3be02f7acd40d91c1e56817ce92cb74259bbd9
guagualeee <zliu921112@gmail.com>
2020-11-22 12:45:06 -0800

    update

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://admin:P@ssw0rd@grahpl-api.blmtu.mongodb.net/test?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://admin:P@ssw0rd@grahpl-api.blmtu.mongodb.net/test?retryWrites=true&w=majority'
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
